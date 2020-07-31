9b1baf8fc9ae0cdd13b83fd7d57cc95a86f725ea
Gryhorii Chepil <gchepil@softserveinc.com>
2020-07-31 13:44:53 +0300

    refactor type

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ class App extends React.Component {
 
         return (
             <Fragment>
-                {JSON.stringify(options)}
+                {JSON.stringify(this.props.sdk.field)}
                 <Select
                     id="optionSelect"
                     name="optionSelect"
